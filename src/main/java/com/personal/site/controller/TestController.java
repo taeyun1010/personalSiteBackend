3ba1279f23c7fa2d398e5c4988faebec4a36ff34
Taeyun Kim <taeyun1010@yahoo.ca>
2021-07-16 17:39:23 +0900

    making route required for ssl certificate verification

--- src/main/java/com/personal/site/controller/TestController.java
+++ src/main/java/com/personal/site/controller/TestController.java
@@ -9,4 +9,11 @@ public class TestController {
     public String getHome() {
         return "test successful";
     }
+
+    @GetMapping("/.well-known/pki-validation/CF787205659CE2C272C67A60D2F0EFCD.txt")
+    public String getText() {
+        return "83876C29A6CE13D27E360EBEEEF5DCF7EDF909C9849E5BA791DCE57D8F0D9EC4\n" +
+                "comodoca.com\n" +
+                "dcv20210716yuv1";
+    }
 }
